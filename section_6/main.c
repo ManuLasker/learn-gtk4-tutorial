c565d6695bfd57e3f31af2657cf6d6b9cd531696
ManuLasker <emanuelule2828@gmail.com>
2023-08-19 21:53:11 -0500

    feat: complete section 5 and section 6

--- section_6/main.c
+++ section_6/main.c
@@ -2,52 +2,63 @@
 #include <stdio.h>
 
 const char *app_id = "org.manulasker.gtk4Learning";
+int wrap_state = 1;
 
 static void
-click1_cb(GtkButton *btn){
-    const char* button_label;
-    button_label = gtk_button_get_label(btn);
-
-    if (g_strcmp0("Hello", button_label) == 0)
-        gtk_button_set_label(btn, "Bye");
-    else
-        gtk_button_set_label(btn, "Hello");
-
-}
-
-static void
-click2_cb(GtkButton *btn, GtkWindow *win){
-    g_print("Bye World Clicked\n");
-    gtk_window_destroy(win);
+wrap_text(GtkButton *btn, GtkTextView *text_view){
+    if (wrap_state){
+        gtk_text_view_set_wrap_mode(text_view, GTK_WRAP_WORD_CHAR);
+        wrap_state = 0;
+    }else{
+        gtk_text_view_set_wrap_mode(text_view, GTK_WRAP_NONE);
+        wrap_state = 1;
+    }
 }
 
 static void
 activate_handler(GApplication *app, gpointer *user_data){
     GtkWidget *win;
+    GtkWidget *s_win;
+    GtkWidget *text_view;
+    GtkWidget *wrap_btn;
     GtkWidget *box;
-    GtkWidget *lab, *button_1, *button_2;
+    GtkTextBuffer *text_buffer;
+
+    gchar *text;
+
+    text =
+        "Once upon a time, there was an old man who was called Taketori-no-Okina. "
+        "It is a japanese word that means a man whose work is making bamboo baskets.\n"
+        "One day, he went into a hill and found a shining bamboo. "
+        "\"What a mysterious bamboo it is!,\" he said. "
+        "He cut it, then there was a small cute baby girl in it. "
+        "The girl was shining faintly. "
+        "He thought this baby girl is a gift from Heaven and took her home.\n"
+        "His wife was surprized at his story. "
+        "They were very happy because they had no children. "
+        ;
 
     win = gtk_application_window_new(GTK_APPLICATION(app));
+    gtk_window_set_title(GTK_WINDOW(win), "taketori");
+    gtk_window_set_default_size(GTK_WINDOW(win), 400, 400);
 
-    gtk_window_set_title(GTK_WINDOW(win), "test window");
-    gtk_window_set_default_size(GTK_WINDOW(win), 500, 600);
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 3);
+    s_win = gtk_scrolled_window_new();
 
-    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 200);
-    gtk_box_set_homogeneous(GTK_BOX(box), TRUE);
+    gtk_box_append(GTK_BOX(box), s_win);
+
+    text_view = gtk_text_view_new();
+    text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+    gtk_text_buffer_set_text(text_buffer, text, -1);
+
+    wrap_btn = gtk_button_new_with_label("wrap text");
+    gtk_box_append(GTK_BOX(box), wrap_btn);
+    g_signal_connect(wrap_btn, "clicked", G_CALLBACK(wrap_text), GTK_TEXT_VIEW(text_view));
+
+    gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(s_win), text_view);
+    gtk_scrolled_window_set_min_content_height(GTK_SCROLLED_WINDOW(s_win), 300);
+
     gtk_window_set_child(GTK_WINDOW(win), box);
-
-    lab = gtk_label_new("Hello World!");
-    gtk_box_append(GTK_BOX(box), lab);
-
-    button_1 = gtk_button_new_with_label("Hello World Button!");
-    gtk_box_insert_child_after(GTK_BOX(box), button_1, lab);
-    g_signal_connect(button_1, "clicked", G_CALLBACK(click1_cb), NULL);
-
-    button_2 = gtk_button_new_with_label("Bye World Button!");
-    gtk_box_insert_child_after(GTK_BOX(box), button_2, button_1);
-    g_signal_connect(button_2, "clicked", G_CALLBACK(click2_cb), win);
-
-
     gtk_window_present(GTK_WINDOW(win));
 }
 
